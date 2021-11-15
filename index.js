b4fd4042a5ea50c1e3f4286fd47440fc38a5b932
John Papa <john@johnpapa.net>
2021-11-14 19:52:29 -0500

    basics of the shopping app with some style. no functionality other than showing the products.

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let express = require('express');
 let app = express();
 let ejs = require('ejs');
-const haikus = require('./haikus.json');
+const shopping = require('./shopping.json');
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', {haikus: haikus});
+  res.render('index', { shopping });
 });
 
 app.listen(port);

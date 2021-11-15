860f04452be0ce5719b08f7567db2079102c7bd2
John Papa <john@johnpapa.net>
2021-11-14 22:37:47 -0500

    rename shopping.json to products.json for consistency

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let express = require('express');
 let app = express();
 // let ejs = require('ejs');
-const shopping = require('./shopping.json');
+const products = require('./products.json');
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', { shopping });
+  res.render('index', { products });
 });
 
 app.listen(port);
